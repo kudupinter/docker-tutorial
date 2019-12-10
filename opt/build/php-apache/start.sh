c22ffb36d26a24ba8864013b40337c5d1f4af97b
kudupinter <58018921+kudupinter@users.noreply.github.com>
2019-12-10 10:19:34 +0700

    Update start.sh
    remove ssl dir mount

--- opt/build/php-apache/start.sh
+++ opt/build/php-apache/start.sh
@@ -7,7 +7,6 @@ docker run --name apache \
   -p 80:80 -p 443:443 \
   -v /opt/conf/apache/php/php.ini:/usr/local/etc/php/php.ini \
   -v /opt/conf/apache/apache2:/etc/apache2 \
-  -v /opt/conf/apache/ssl:/etc/ssl/certificates \
   -v /home/www/html:/var/www/html \
   --log-driver json-file \
   --log-opt max-size=50m \
